2bbc5de699b918acab0675512162206363fbe6e1
Pradeep Sethi <psethi@gmail.com>
2013-01-31 11:24:19 +0530

    added function to generate random colors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -122,6 +122,25 @@ MONTHS = { 1 => "Jan", 2 => "Feb", 3 => "Mar", 4 => "Apr",
     MONTHS[month_num]
   end
   
+  def self.hsv_to_rgb(h, s, v)
+    h_i = (h*6).to_i
+    f = h*6 - h_i
+    p = v * (1 - s)
+    q = v * (1 - f*s)
+    t = v * (1 - (1 - f) * s)
+    r, g, b = v, t, p if h_i==0
+    r, g, b = q, v, p if h_i==1
+    r, g, b = p, v, t if h_i==2
+    r, g, b = p, q, v if h_i==3
+    r, g, b = t, p, v if h_i==4
+    r, g, b = v, p, q if h_i==5
+    color = sprintf "#%02x%02x%02x",
+     (r*256).to_i, (g*256).to_i, (b*256).to_i
+     logger.debug("color here")
+     logger.debug(color)
+     return color
+  end
+
   private
     def authenticate
       authenticate_or_request_with_http_digest(TC_REALM) do |username|
@@ -129,4 +148,7 @@ MONTHS = { 1 => "Jan", 2 => "Feb", 3 => "Mar", 4 => "Apr",
       end
     end
  
+ 
+ 
+    
 end
